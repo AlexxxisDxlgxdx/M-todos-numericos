b2ca126574c1bc57d9c86cf818f3501b584a4763
AlexxxisDxlgxdx <alexis.jadp@gmail.com>
2024-09-14 21:33:37 -0500

    Update error.py

--- error.py
+++ error.py
@@ -1,67 +1,54 @@
 import numpy as np
-from numpy.polynomial import Polynomial
+import sympy as sp
+import math
 
-# x: Lista de los nodos de la interpolación
-# y: Lista de los valores de la interpolación
-# return:
-#   itr: El polinomio de interpolación
-#   L: Lista que contiene los polinomios base
-def lagrange(x, y):
-    n = len(x)
-    # Calculamos los polinomios base
-    L = []
-    itr = Polynomial(0)
-    for i in range(n):
-        base = Polynomial([1])
-        for j in range(n):
-            if j != i:
-                factor = Polynomial([-x[j], 1])/(x[i]-x[j])
-                base = base * factor
-        L.append(base)
-        itr += y[i] * base
-    return itr, L
+# f_sym: Es una función simbólica de variable 'x' que representa la función a aproximar
+# nodes: Lista que contiene los nodos de la interpolación
+# xs: Lista de puntos donde evaluar el error
+# error_bound: Lista de las cotas de error
+def general_error_bound(f_sym, nodes, xs):
+    n = len(nodes)
+    a = min(nodes)
+    b = max(nodes)
+    x_sym = sp.symbols('x')
 
-# x: Lista de los nodos de la interpolación
-# y: Lista de los valores de la interpolación
-# return:  
-#   itr: El polinomio de interpolación
-#   N: Matriz que representa la tabla de diferencias divididas
-def newton(x, y):
-    n = len(x)
-    N = np.zeros((n, n))
-    # Completamos la tabla de diferencias divididas
-    N[0,:] = y
-    for i in range(1,n):
-        for j in range(n-i):
-            top = N[i-1,j] - N[i-1,j+1]
-            bottom = x[j] - x[j+i]
-            N[i,j] = top/bottom
-    # Calculamos los polinomios base
-    itr = Polynomial(0)
-    for i in range(n):
-        base = Polynomial([1])
-        for j in range(i):
-            factor = Polynomial([-x[j], 1])
-            base = base * factor
-        itr += N[i,0] * base
-    return itr, N
+    # Calculamos la n derivada de la función
+    nth_derivative = sp.diff(f_sym, x_sym, n)
 
-def forward_differences(Y):
-    n = len(Y)
-    F = np.zeros((n,n))
-    F[0,:] = Y
-    for i in range(1,n):
-        for j in range(0, n-i):
-            diff = F[i-1,j+1] - F[i-1,j]
-            F[i,j] = diff
-    return F
+    # Calculamos las cota de error
+    M = sp.maximum(nth_derivative, x_sym, sp.Interval(a, b)).evalf()
+    error_bound = []
+    for elem in xs:
+        P = 1
+        for i in range(n):
+            P *= abs(elem - nodes[i])
+        error_bound.append(P*M/math.factorial(n))
+    return error_bound
 
-def backward_differences(Y):
-    n = len(Y)
-    F = np.zeros((n,n))
-    F[0,:] = Y
-    for i in range(1,n):
-        for j in range(i, n):
-            diff = F[i-1,j] - F[i-1,j-1]
-            F[i,j] = diff
-    return F
+# f_sym: Es una función simbólica de variable 'x' que representa la función a aproximar
+# n: Es el número de nodos que se van a utilizar en la interpolación
+# a, b: Intervalo de la interpolación
+# error_bound: La cota del error para una interpolación con nodos igualmente espaciados
+def evenly_error_bound(f_sym, n, a, b):
+    x_sym = sp.symbols('x')
+
+    # Calculamos la n derivada de la función
+    nth_derivative = sp.diff(f_sym, x_sym, n)
+    
+    # Calculamos la cota de error
+    M = sp.maximum(nth_derivative, x_sym, sp.Interval(a, b)).evalf()
+    h = (b-a)/(n-1)
+    error_bound = h**n*M/(4*n)
+    
+    return error_bound
+
+def m_error_bound(f_sym, n, a, b):
+    x_sym = sp.symbols('x')
+
+    # Calculamos la n derivada de la función
+    nth_derivative = sp.diff(f_sym, x_sym, n)
+    
+    # Calculamos la cota de error
+    M = sp.maximum(nth_derivative, x_sym, sp.Interval(a, b)).evalf()
+    
+    return M
