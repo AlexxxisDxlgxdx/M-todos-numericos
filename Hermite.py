ba0c0a11a06d5e06e8526a1cd2a68e3c1a94b9ad
AlexxxisDxlgxdx <alexis.jadp@gmail.com>
2024-09-14 21:34:47 -0500

    Update Hermite.py

--- Hermite.py
+++ Hermite.py
@@ -1,67 +1,68 @@
 import numpy as np
 from numpy.polynomial import Polynomial
+from .interpolation import lagrange
 
-# x: Lista de los nodos de la interpolación
-# y: Lista de los valores de la interpolación
+# X: Lista de los nodos de la interpolación
+# Y: Lista de los valores de la interpolación
+# DY: Lista de las derivadas de la interpolación
 # return:
 #   itr: El polinomio de interpolación
-#   L: Lista que contiene los polinomios base
-def lagrange(x, y):
-    n = len(x)
-    # Calculamos los polinomios base
-    L = []
-    itr = Polynomial(0)
+#   H, DH: Listas que contienen los polinomios base
+def hermite(X, Y, DY):
+    n = len(X)
+    _, L = lagrange(X, Y)
+    DL = [p.deriv() for p in L]
+    H = []
+    S1 = Polynomial(0)
     for i in range(n):
-        base = Polynomial([1])
-        for j in range(n):
-            if j != i:
-                factor = Polynomial([-x[j], 1])/(x[i]-x[j])
-                base = base * factor
-        L.append(base)
-        itr += y[i] * base
-    return itr, L
+        r = (Polynomial(1)-2*Polynomial([-X[i],1])*DL[i](X[i]))*L[i]**2
+        S1 += Y[i]*r
+        H.append(r)
 
-# x: Lista de los nodos de la interpolación
-# y: Lista de los valores de la interpolación
-# return:  
+    DH = []
+    S2 = Polynomial(0)
+    for i in range(n):
+        r = Polynomial([-X[i],1])*L[i]**2
+        S2 += DY[i]*r
+        DH.append(r)
+    itr = S1 + S2
+    return itr, H, DH
+
+# X: Lista de los nodos de la interpolación
+# Y: Lista de los valores de la interpolación
+# DY: Lista de las derivadas de la interpolación
+# return:
 #   itr: El polinomio de interpolación
-#   N: Matriz que representa la tabla de diferencias divididas
-def newton(x, y):
-    n = len(x)
-    N = np.zeros((n, n))
-    # Completamos la tabla de diferencias divididas
-    N[0,:] = y
-    for i in range(1,n):
-        for j in range(n-i):
-            top = N[i-1,j] - N[i-1,j+1]
-            bottom = x[j] - x[j+i]
-            N[i,j] = top/bottom
+#   H: La tabla de diferencias divididas
+def hermite_table(X, Y, DY):
+    n = len(X)
+    # Rellenamos el array de los z_i
+    Z = np.zeros(2*n)
+    for i in range(n):
+        Z[2*i:2*i+2] = [X[i], X[i]]
+    # Creamos la tabla de diferencias divididas
+    H = np.zeros((2*n, 2*n))
+    # Rellenamos con los valores
+    for i in range(n):
+        H[0, 2*i:2*i+2] = [Y[i], Y[i]]
+    # Rellenamos con las derivadas
+    for i in range(n):
+        H[1, 2*i] = DY[i]
+    # Rellenamos con las diferencias divididas
+    for i in range(1, 2*n):
+        for j in range(2*n-i):
+            if i == 1 and j % 2 == 0:
+                continue
+            top = H[i-1,j] - H[i-1,j+1]
+            bottom = Z[j] - Z[j+i]
+            H[i,j] = top/bottom
     # Calculamos los polinomios base
     itr = Polynomial(0)
-    for i in range(n):
+    for i in range(2*n):
         base = Polynomial([1])
         for j in range(i):
-            factor = Polynomial([-x[j], 1])
+            factor = Polynomial([-Z[j], 1])
             base = base * factor
-        itr += N[i,0] * base
-    return itr, N
-
-def forward_differences(Y):
-    n = len(Y)
-    F = np.zeros((n,n))
-    F[0,:] = Y
-    for i in range(1,n):
-        for j in range(0, n-i):
-            diff = F[i-1,j+1] - F[i-1,j]
-            F[i,j] = diff
-    return F
-
-def backward_differences(Y):
-    n = len(Y)
-    F = np.zeros((n,n))
-    F[0,:] = Y
-    for i in range(1,n):
-        for j in range(i, n):
-            diff = F[i-1,j] - F[i-1,j-1]
-            F[i,j] = diff
-    return F
+        itr += H[i,0] * base
+    
+    return itr, H
